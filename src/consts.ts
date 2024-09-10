e902e2951d2f9b9bc3bbebba5a9538fe8ea2d51c
Tiffany Rossi <tiffanyrossi@gmail.com>
2024-09-09 22:40:53 -0300

    criada pagina de checklist

--- src/consts.ts
+++ src/consts.ts
@@ -83,6 +83,10 @@ export const NAV_LINKS: Array<{ title: string, path: string }> = [
     path: '/blogroll'
   },
   {
+    title: 'checklist',
+    path: '/checklist'
+  },
+  {
     title: "arquivo",
     path: '/arquivo'
   }
